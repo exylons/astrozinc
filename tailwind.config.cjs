dabe7501968c61885281d64979f98a5218b962f4
Lance Ross <145350865+exylons@users.noreply.github.com>
2024-07-02 14:31:20 +0800

    add dark mode, custom scrollbar and toggle via class

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,astro}"],
-  darkMode: "media",
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
